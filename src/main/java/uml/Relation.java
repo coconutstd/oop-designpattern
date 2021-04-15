87fb3c8e1fbecfaead799854228e72f7cb3ad039
junui <wnsspdl@naver.com>
2021-04-15 20:27:19 +0900

    Initial commit

---
+++ src/main/java/uml/Relation.java
@@ -0,0 +1,22 @@
+package uml;
+
+class Student{
+    private Course course;
+
+    public Student(){
+
+    }
+
+}
+
+class Course{
+    private Student student;
+
+    public Course(){
+
+    }
+}
+
+public class Relation {
+
+}
