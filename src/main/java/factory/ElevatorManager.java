f9b2fba9e92047e86d82fce7c0d08b14477f3a0a
junui <wnsspdl@naver.com>
2021-05-19 22:45:09 +0900

    동적 스케줄링을 지원하는 ElevatorManager 클래스

--- src/main/java/factory/ElevatorManager.java
+++ src/main/java/factory/ElevatorManager.java
@@ -4,21 +4,32 @@ import java.util.*;
 
 public class ElevatorManager {
     private List<ElevatorController> controllers;
-    private ThroughputScheduler scheduler;
 
     public ElevatorManager(int controllerCount) {
         controllers = new ArrayList<ElevatorController>(controllerCount);
         for (int i = 0; i < controllerCount; ++i) {
-            ElevatorController controller = new ElevatorController(i);
+            ElevatorController controller = new ElevatorController(i + 1);
             controllers.add(controller);
         }
-        scheduler = new ThroughputScheduler();
     }
 
     void requestElevator(int destination, Direction direction) {
+        ElevatorScheduler scheduler;
+
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (hour < 12)
+            scheduler = new ResponseTimeScheduler();
+        else
+            scheduler = new ThroughputScheduler();
+
         int selectedElevator = scheduler.selectElevator(this, destination, direction);
         controllers.get(selectedElevator).gotoFloor(destination);
     }
+}
+
+interface ElevatorScheduler {
+    public abstract int selectElevator(ElevatorManager manager, int destination, Direction direction);
 }
 
 class ElevatorController {
@@ -38,7 +49,15 @@ class ElevatorController {
     }
 }
 
-class ThroughputScheduler {
+class ResponseTimeScheduler implements ElevatorScheduler {
+    @Override
+    public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
+        return 0;
+    }
+}
+
+class ThroughputScheduler implements ElevatorScheduler{
+    @Override
     public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
         return 0;
     }
