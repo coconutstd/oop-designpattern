8aea82eca5e08445f3462a3da7bea3a289453bc6
junui <wnsspdl@naver.com>
2021-05-19 22:58:50 +0900

    팩토리 메서드 패턴

--- src/main/java/factory/ElevatorManager.java
+++ src/main/java/factory/ElevatorManager.java
@@ -2,27 +2,47 @@ package factory;
 
 import java.util.*;
 
+enum SchedulingStategyID { RESPONSE_TIME, THROUGHTPUT, DYNAMIC }
+enum Direction { UP, DOWN }
+
+class SchedulerFactory {
+    public static ElevatorScheduler getScheduler(SchedulingStategyID stategyID) {
+        ElevatorScheduler scheduler = null;
+        switch (stategyID) {
+            case RESPONSE_TIME:
+                scheduler = new ResponseTimeScheduler();
+                break;
+            case THROUGHTPUT:
+                scheduler = new ThroughputScheduler();
+                break;
+            case DYNAMIC:
+                int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                if (hour < 12)
+                    scheduler = new ResponseTimeScheduler();
+                else
+                    scheduler = new ThroughputScheduler();
+                break;
+        }
+        return scheduler;
+    }
+}
+
 public class ElevatorManager {
     private List<ElevatorController> controllers;
+    private SchedulingStategyID stategyID;
 
-    public ElevatorManager(int controllerCount) {
+    public ElevatorManager(int controllerCount, SchedulingStategyID stategyID) {
         controllers = new ArrayList<ElevatorController>(controllerCount);
         for (int i = 0; i < controllerCount; ++i) {
             ElevatorController controller = new ElevatorController(i + 1);
             controllers.add(controller);
         }
+        this.stategyID = stategyID;
     }
 
     void requestElevator(int destination, Direction direction) {
-        ElevatorScheduler scheduler;
-
-        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-
-        if (hour < 12)
-            scheduler = new ResponseTimeScheduler();
-        else
-            scheduler = new ThroughputScheduler();
-
+        ElevatorScheduler scheduler = SchedulerFactory.getScheduler(stategyID);
+        System.out.println(scheduler);
         int selectedElevator = scheduler.selectElevator(this, destination, direction);
         controllers.get(selectedElevator).gotoFloor(destination);
     }
@@ -63,5 +83,19 @@ class ThroughputScheduler implements ElevatorScheduler{
     }
 }
 
-class Direction {
+
+
+
+class Client {
+    public static void main(String[] args) {
+        ElevatorManager emWithResponseTimeScheduler = new ElevatorManager(2, SchedulingStategyID.RESPONSE_TIME);
+        emWithResponseTimeScheduler.requestElevator(10, Direction.UP);
+
+        ElevatorManager emWithThroughputScheduler = new ElevatorManager(2, SchedulingStategyID.THROUGHTPUT);
+        emWithThroughputScheduler.requestElevator(10, Direction.UP);
+
+        ElevatorManager emWithDynamicScheduler = new ElevatorManager(2, SchedulingStategyID.DYNAMIC);
+        emWithDynamicScheduler.requestElevator(10, Direction.UP);
+
+    }
 }
