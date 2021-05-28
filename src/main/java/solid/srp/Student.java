b786cd432ef8ed19da8ae0f08390efa88bf341c6
junui <wnsspdl@naver.com>
2021-05-28 19:28:01 +0900

    많은 책임을 가진 Student 클래스

---
+++ src/main/java/solid/srp/Student.java
@@ -0,0 +1,16 @@
+package solid.srp;
+
+/*
+현재 학생 클래스는 많은 책임을 수행하고 있다.
+수강 과목 추가, 조회만 해도 충분하다.
+데이터베이스 관련 로직은 다른 클래스에서 해도 된다.
+ */
+
+public class Student {
+    public void getCourses() { }
+    public void addCourse(Course c) { }
+    public void save() { }
+    public Student load() { }
+    public void printOnReport() { }
+    public void printOnAttendanceBook() { }
+}
